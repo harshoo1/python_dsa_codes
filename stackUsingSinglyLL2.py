9e272bd496b0c25e988294036e73f62f3125b458
harshoo1 <harshsharma11d@gmail.com>
2024-12-16 00:01:04 +0530

    stack using singly ll class

--- stackUsingSinglyLL2.py
+++ stackUsingSinglyLL2.py
@@ -1,5 +1,19 @@
 """ stack using inheriting the linked list class 
 singly ll as parent class and stack as child class"""
 
-from sll_package.sll_class import SinglyLL
+from sll_package.sll_class import *
+ 
+ #1. defining stack class
+class Stack:
+    def __init__(self):
+        self.mylist = SinglyLL()
+#2. is_empty() ?
+    def is_empty(self):
+        self.mylist.isEmpty()
+#3. push data
+    def push(self,data):
+        self.mylist.insertAtStart()
+#4. pop data        
+   
+
 
