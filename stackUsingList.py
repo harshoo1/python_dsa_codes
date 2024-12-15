68b913b20eac01b0151aca194958597277b23370
harshoo1 <harshsharma11d@gmail.com>
2024-12-15 22:25:13 +0530

    implementation of stack using list

---
+++ stackUsingList.py
@@ -0,0 +1,22 @@
+""" STACK """
+
+#1. stack using list
+class Stack:
+    def __init__(self):
+        self.value = []
+#2. check if stack is_empty() ?
+    def is_empty(self):
+        return len(self.value) == 0
+#3. push() method for stack
+    def push(self,data):
+        self.value.append(data)
+#4. pop() method for stack
+    def pop(self):
+        if not self.is_empty()
+#5. peek() method for stack
+#6. check size of stack.  size()
+
+
+
+
+
