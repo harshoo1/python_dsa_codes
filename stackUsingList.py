b40ab56e1145f3e69a90c57adcf7cb0ec667c7e3
harshoo1 <harshsharma11d@gmail.com>
2024-12-15 22:37:48 +0530

    stack using list

--- stackUsingList.py
+++ stackUsingList.py
@@ -12,11 +12,34 @@ class Stack:
         self.value.append(data)
 #4. pop() method for stack
     def pop(self):
-        if not self.is_empty()
+        if not self.is_empty():
+            return self.value.pop()
+        else:
+            raise IndexError("Empty Stack")
 #5. peek() method for stack
+    def peek(self):
+        if not self.is_empty():
+            return self.value[-1]
+        else:
+            raise IndexError("Empty Stack")
 #6. check size of stack.  size()
+    def size(self):
+        return len(self.value)
 
 
 
+#main code
+stack1 = Stack()
+stack1.push(10)
+stack1.push(20)
+stack1.push(30)
+stack1.push(40)
+stack1.push(50)
 
+
+print(stack1.peek())
+stack1.pop()
+print(stack1.peek())
+
+# we can't use iterator here to access elements in stack
 
